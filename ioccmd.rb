8e93c4a82cdc3820c88603b4356e88cd62aea950
Matteo Michelini <matteo.michelini@gmail.com>
2013-04-28 15:21:23 +0100

    added parsing of nodes and attributes

--- ioccmd.rb
+++ ioccmd.rb
@@ -4,6 +4,24 @@ require 'optparse'
 require 'rexml/document'
 include REXML
 
+
+def attributes(xmlfile)
+  nodes_list = nodes(xmlfile)
+  nodes_unique = nodes_list.uniq
+  nodes_unique.each do |item|
+    puts "#{item}: "
+    puts nodes_list.select {|a| a === "#{item}"}.count
+  end
+end
+
+def nodes(xmlfile)
+  node_list = []
+  XPath.each(xmlfile, "//Context") do |item|
+    node_list << item.attribute("document").value
+  end
+  return node_list
+end
+
 def domainName(xmlfile)
   XPath.each(xmlfile, "//Context[@search='Network/DNS']/ancestor::IndicatorItem") do |item|
     value = item.attribute("id").value
@@ -26,7 +44,8 @@ optparse = OptionParser.new do |opts|
                 "Example: ioccmd -f myindicator.xml -e IP,DOMAIN\n" \
                 "List of Objects:\n" \
                 "IP      - extract remote IPs\n" \
-                "DOMAIN  - extract remote Domains\n" 
+                "DOMAIN  - extract remote Domains\n" \
+                "ATTRS   - extract list of Attributes\n"
   opts.on('-h', '--help', 'Display this menu') do
     puts opts
     exit
@@ -61,6 +80,11 @@ options[:extract].each do |argument|
   if argument === "DOMAIN"
     puts "[+] List of Remote Domains:"
     domainName(xmlioc)
+    puts "[+] ==== \n\n"
+  end
+  if argument === "ATTRS"
+    puts "[+] List of Attributes:"
+    attributes(xmlioc)
     puts "[+] ==== \n\n"
   end
 end

6e79595bafda78989e74ad2538a333f406264376
Matteo Michelini <matteo.michelini@gmail.com>
2013-04-28 15:53:50 +0100

    added parsing of values inside attribute types and overall license

--- ioccmd.rb
+++ ioccmd.rb
@@ -1,8 +1,37 @@
 #!/usr/bin/env ruby
-
+## Copyright (c) 2013 Matteo Michelini - cor3ngine
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
 require 'optparse'
 require 'rexml/document'
 include REXML
+
+def attr_value(xmlfile,type)
+  XPath.each(xmlfile, "//Context[@search='#{type}']/ancestor::IndicatorItem") do |item|
+    value = item.attribute("id").value
+    puts XPath.match(xmlfile, "//IndicatorItem[@id='#{value}']/Content").map {|el| el.text}
+  end  
+end
 
 def type(xmlfile,attribute)
   attr_types = []
@@ -67,6 +96,9 @@ optparse = OptionParser.new do |opts|
   opts.on('-t', '--type [ATTRIBUTE,...]', Array, 'Extract TYPE for each ATTRIBUTE') do |f|
     options[:type] = f
   end
+  opts.on('-v', '--value VALUE', 'Extract VALUE for TYPE of ATTRIBUTE') do |f|
+    options[:value] = f
+  end
 end
 
 optparse.parse!
@@ -102,4 +134,8 @@ if !options[:type].nil?
   options[:type].each do |argument|
     type(xmlioc,argument)
   end
+end
+
+if !options[:value].nil?
+  attr_value(xmlioc,options[:value])  
 end
